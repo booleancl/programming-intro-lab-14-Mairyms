c10ca63f9e0fa312933af73af647708b8930616c
Mairym Salcedo <mairymsalcedo@gmail.com>
2022-10-05 20:26:13 -0300

    Se creo la data de la base de dato

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,11 @@
+COPY users (email)
+FROM '/Users/mairym/Clases-de-programacion/clase-5-oct/programming-intro-lab-14-Mairyms/data/users.csv'
+DELIMITER ',' CSV HEADER;
 
+COPY posts (id,users_id,title,date)
+FROM '/Users/mairym/Clases-de-programacion/clase-5-oct/programming-intro-lab-14-Mairyms/data/posts.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY comments (id,user_id,post_id,content,date)
+FROM '/Users/mairym/Clases-de-programacion/clase-5-oct/programming-intro-lab-14-Mairyms/data/comentarios.csv'
+DELIMITER ',' CSV HEADER;
